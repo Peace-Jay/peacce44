7045669de86edd8ed6ecf8d2f91fa632ef5202f6
peace jay <alebiosupeace@gmail.com>
2023-11-10 08:13:50 -0800

    Test

---
+++ assign.py
@@ -0,0 +1,7 @@
+students = ["peace",  "joy",  "favour",  "precious",  "mercy"]
+I_D = ["baf/003", "baf/001", "baf/002", "baf/004", "baf/001"]
+new_students = input("what is your name?")
+students.insert(5, new_students)
+print(students)
+I_D.insert(4, "baf/006")
+print(I_D)
